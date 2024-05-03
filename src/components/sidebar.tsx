458fa06170178bc00640b71bd6146bac693eff64
Krishna <pulluru.krishnareddy@mileseducation.com>
2024-05-04 02:31:24 +0530

    logos fix

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react'
-import { IconChevronsLeft, IconMenu2, IconX } from '@tabler/icons-react'
-import { Layout, LayoutHeader } from './custom/layout'
-import { Button } from './custom/button'
-import Nav from './nav'
-import { cn } from '@/lib/utils'
-import { sidelinks } from '@/data/sidelinks'
+import { useEffect, useState } from "react";
+import { IconChevronsLeft, IconMenu2, IconX } from "@tabler/icons-react";
+import { Layout, LayoutHeader } from "./custom/layout";
+import { Button } from "./custom/button";
+import Nav from "./nav";
+import { cn } from "@/lib/utils";
+import { sidelinks } from "@/data/sidelinks";
 
 interface SidebarProps extends React.HTMLAttributes<HTMLElement> {
-  isCollapsed: boolean
-  setIsCollapsed: React.Dispatch<React.SetStateAction<boolean>>
+  isCollapsed: boolean;
+  setIsCollapsed: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function Sidebar2({
@@ -16,45 +16,53 @@ export default function Sidebar2({
   isCollapsed,
   setIsCollapsed,
 }: SidebarProps) {
-  const [navOpened, setNavOpened] = useState(false)
+  const [navOpened, setNavOpened] = useState(false);
 
   /* Make body not scrollable when navBar is opened */
   useEffect(() => {
     if (navOpened) {
-      document.body.classList.add('overflow-hidden')
+      document.body.classList.add("overflow-hidden");
     } else {
-      document.body.classList.remove('overflow-hidden')
+      document.body.classList.remove("overflow-hidden");
     }
-  }, [navOpened])
+  }, [navOpened]);
 
   return (
     <aside
       className={cn(
-        `fixed left-0 right-0 top-0 z-50 w-full border-r-2 border-r-muted transition-[width] md:bottom-0 md:right-auto md:h-svh ${isCollapsed ? 'md:w-14' : 'md:w-64'}`,
+        `fixed left-0 right-0 top-0 z-50 w-full border-r-2 border-r-muted transition-[width] md:bottom-0 md:right-auto md:h-svh ${isCollapsed ? "md:w-14" : "md:w-64"}`,
         className
       )}
     >
       {/* Overlay in mobile */}
       <div
         onClick={() => setNavOpened(false)}
-        className={`absolute inset-0 transition-[opacity] delay-100 duration-700 ${navOpened ? 'h-svh opacity-50' : 'h-0 opacity-0'} w-full bg-black md:hidden`}
+        className={`absolute inset-0 transition-[opacity] delay-100 duration-700 ${navOpened ? "h-svh opacity-50" : "h-0 opacity-0"} w-full bg-black md:hidden`}
       />
 
       <Layout>
         {/* Header */}
-        <LayoutHeader className='sticky top-0 justify-between px-4 py-3 md:px-4'>
+        <LayoutHeader className="sticky top-0 justify-between px-4 py-3 md:px-4">
           <div>
             {/* Logo */}
-            <img src='/logo.png' className='w-40 h-11 drop-shadow-xl'/>
+            {!isCollapsed && (
+              <img src="/logos/logo.png" className=" h-10 drop-shadow-xl" />
+            )}
+            {isCollapsed && (
+              <img
+                src="/logos/logo-sm.png"
+                className=" h-10 drop-shadow-xl"
+              />
+            )}
           </div>
 
           {/* Toggle Button in mobile */}
           <Button
-            variant='ghost'
-            size='icon'
-            className='md:hidden'
-            aria-label='Toggle Navigation'
-            aria-controls='sidebar-menu'
+            variant="ghost"
+            size="icon"
+            className="md:hidden"
+            aria-label="Toggle Navigation"
+            aria-controls="sidebar-menu"
             aria-expanded={navOpened}
             onClick={() => setNavOpened((prev) => !prev)}
           >
@@ -64,8 +72,8 @@ export default function Sidebar2({
 
         {/* Navigation links */}
         <Nav
-          id='sidebar-menu'
-          className={`h-full flex-1 overflow-auto ${navOpened ? 'max-h-screen' : 'max-h-0 py-0 md:max-h-screen md:py-2'}`}
+          id="sidebar-menu"
+          className={`h-full flex-1 overflow-auto ${navOpened ? "max-h-screen" : "max-h-0 py-0 md:max-h-screen md:py-2"}`}
           closeNav={() => setNavOpened(false)}
           isCollapsed={isCollapsed}
           links={sidelinks}
@@ -74,16 +82,16 @@ export default function Sidebar2({
         {/* Scrollbar width toggle button */}
         <Button
           onClick={() => setIsCollapsed((prev) => !prev)}
-          size='icon'
-          variant='outline'
-          className='absolute -right-5 top-1/2 hidden rounded-full md:inline-flex'
+          size="icon"
+          variant="outline"
+          className="absolute -right-5 top-1/2 hidden rounded-full md:inline-flex"
         >
           <IconChevronsLeft
             stroke={1.5}
-            className={`h-5 w-5 ${isCollapsed ? 'rotate-180' : ''}`}
+            className={`h-5 w-5 ${isCollapsed ? "rotate-180" : ""}`}
           />
         </Button>
       </Layout>
     </aside>
-  )
+  );
 }
